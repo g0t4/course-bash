087cbd946918b2312732ae0b1defc6e031422976
Wes Higbee <wes.mcclure@gmail.com>
2025-08-02 13:04:44 -0500

    if

--- flow/process.sh
+++ flow/process.sh
@@ -1,4 +1,6 @@
-test -f cpiai.csv || wget 'https://datahub.io/core/cpi-us/_r/-/data/cpiai.csv'
+if test ! -f cpiai.csv; then
+    wget 'https://datahub.io/core/cpi-us/_r/-/data/cpiai.csv'
+fi
 
 head cpiai.csv
 
