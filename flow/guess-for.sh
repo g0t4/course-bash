1e8f902ebc1c6fc2c29f2d86626d2b64b7ac0b26
Wes Higbee <wes.mcclure@gmail.com>
2025-08-02 21:03:15 -0500

    guess-for game

---
+++ flow/guess-for.sh
@@ -0,0 +1,14 @@
+target=$((RANDOM % 10))
+
+# echo $target
+
+echo "Guess a number from 0 to 9, three tries!"
+for ((i = 1; i <= 3; i++)); do
+    read -p "guess $i: " guess
+
+    if ((guess == target)); then
+        echo "CONGRATULATIONS! $target is the right number!"
+    fi
+done
+
+
