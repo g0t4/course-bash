10ce34172cbd8eac9c3aa40606ff642b5670cfdc
Wes Higbee <wes.mcclure@gmail.com>
2025-08-02 21:42:08 -0500

    duplicate for a while example

---
+++ flow/guess-while.sh
@@ -0,0 +1,23 @@
+target=$((RANDOM % 10))
+
+echo "target: $target"
+echo
+
+echo "Guess a number from 0 to 9, three tries!"
+for ((i = 1; i <= 3; i++)); do
+    read -p "guess $i: " guess
+
+    if ! [[ $guess =~ ^[0-9]$ ]]; then
+        echo "Please enter a single digit number!"
+        ((i--)) # allow retry
+        continue
+    fi
+
+    if ((guess == target)); then
+        echo "CONGRATULATIONS! $target is the right number!"
+        break
+    fi
+    echo "Nope!"
+done
+
+
