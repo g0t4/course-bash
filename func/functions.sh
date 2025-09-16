abf79cce18955e03bf7e03edf77cbe3d4267f8aa
Wes Higbee <wes.mcclure@gmail.com>
2025-09-16 18:34:02 -0500

    $@

--- func/functions.sh
+++ func/functions.sh
@@ -1,9 +1,10 @@
 function help_bat() {
-    # help "$1" | bat -l help
-    echo "1: $1"
-    echo "2: $2"
-    echo "3: $3"
-    echo "4: $4"
-    echo "@: $@"
-    echo "#: $#"
+    help "$@" | bat -l help
+    help "$1" "$2" "$3" "$4" "$5" | bat -l help
+    # echo "1: $1"
+    # echo "2: $2"
+    # echo "3: $3"
+    # echo "4: $4"
+    # echo "@: $@"
+    # echo "#: $#"
 }
