1f596cdf7f750f7a84ac9a00a48df652fbfd56ef
Wes Higbee <wes.mcclure@gmail.com>
2025-09-16 22:13:53 -0500

    notes

--- func/repos.sh
+++ func/repos.sh
@@ -1,33 +1,39 @@
 function is_git_repo {
-    # ignore STDOUT/STDERR if git is missing OR not in work tree (repo)
+    # suppress STDOUT/STDERR
+    # use exit status to communicate success/failure
     git rev-parse --is-inside-work-tree 1>/dev/null 2>&1
 }
 
 function is_hg_repo {
-    # ignore STDOUT/STDERR if hg command is missing OR not in hg repo
+    # suppress STDOUT/STDERR
+    # use exit status to communicate success/failure
     hg root >/dev/null 2>&1
 }
 
 function git_repo_root {
+    # suppress STDERR
+    # preserve STDOUT (repo path)
     git rev-parse --show-toplevel 2>/dev/null
     # return 0
 }
 
 function hg_repo_root {
+    # suppress STDERR
+    # preserve STDOUT (repo path)
     hg root 2>/dev/null
 }
 
 function _repo_root() {
 
     if is_git_repo; then
-        git_repo_root
+        git_repo_root # STDOUT
     elif is_hg_repo; then
-        hg_repo_root
+        hg_repo_root # STDOUT
     else
-        # warn over STDERR (that way, cd $(_repo_root) still works)
-        echo "cannot find repo root" >&2
-        builtin pwd
+        echo "cannot find repo root" >&2 # STDERR, so cd $(_repo_root) still works
+        builtin pwd                      # STDOUT
     fi
 }
 
 alias cdr='cd "$(_repo_root)"'
+# (set -x; cd "$(_repo_root)" )
