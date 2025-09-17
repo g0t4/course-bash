637671956d652d262728c5dcf9d7179899ba7e8d
Wes Higbee <wes.mcclure@gmail.com>
2025-09-16 21:31:14 -0500

    extract functions

--- func/repos.sh
+++ func/repos.sh
@@ -1,11 +1,27 @@
+function is_git_repo {
+    git rev-parse --is-inside-work-tree 1>/dev/null 2>&1
+}
+
+function is_hg_repo {
+    hg root >/dev/null 2>&1
+}
+
+function git_repo_root {
+    git rev-parse --show-toplevel 2>/dev/null
+    # return 0
+}
+
+function hg_repo_root {
+    # ignore STDOUT/STDERR if hg command is missing OR not in hg repo
+    hg root 2>/dev/null
+}
+
 function _repo_root() {
 
-    if git rev-parse --is-inside-work-tree 1>/dev/null 2>&1; then
-        # ignore STDOUT/STDERR if git is missing OR not in work tree (repo)
-        git rev-parse --show-toplevel 2>/dev/null
-    elif hg root >/dev/null 2>&1; then
-        # ignore STDOUT/STDERR if hg command is missing OR not in hg repo
-        hg root 2>/dev/null
+    if is_git_repo; then
+        git_repo_root
+    elif is_hg_repo; then
+        hg_repo_root
     else
         # warn over STDERR (that way, cd $(_repo_root) still works)
         echo "cannot find repo root" >&2
