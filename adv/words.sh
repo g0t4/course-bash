946941727880243531829541f9c09417f00cb079
Wes Higbee <wes.mcclure@gmail.com>
2025-09-17 03:05:24 -0500

    copy of adder as we left it

--- adv/words.sh
+++ adv/words.sh
@@ -1,3 +1,44 @@
+function adder() {
+    echo "\$#: $#"
+    local -i sum=0
+    for param in "$@"; do
+        echo $param
+        sum+=$param
+    done
+    echo "sum: $sum"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function show_words() {
     # preserves passed words (no new splitting)
     #  think of this is "show your words"
